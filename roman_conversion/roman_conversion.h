6003995bfecd48d9875071e680ead7720844c31f
chelvam244 <chelvam244@yahoo.com>
2016-06-26 12:28:42 +0530

    Added roman_conversion C function to header file

--- roman_conversion/roman_conversion.h
+++ roman_conversion/roman_conversion.h
@@ -1,3 +1,5 @@
 int roman2dec (char * str_num);
 int get_roman_value (char c);
 char * dec2roman (int num);
+char * calc_parser(void);
+char * roman_operation(char * str_operand1, char * str_operand2, char arith);
