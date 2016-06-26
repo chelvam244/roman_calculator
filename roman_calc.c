b8e85cd9988757788dd18cb0e5c32965d60abac0
chelvam244 <chelvam244@yahoo.com>
2016-06-26 12:22:32 +0530

    roman_calc.c - It is not an important file and is currently used for debugging purpose. Make file is this folder generates the executable file from this C file. The operands and the airthmetic operators for Roman Calc are hard-coded in this C file.

--- roman_calc.c
+++ roman_calc.c
@@ -15,9 +15,12 @@
 #include "roman_conversion.h"
 
 void main() {
-	char str_operand1[255];
-	char chr_arith;
-	char str_operand2[255];
+	//char str_operand1[255];
+	//char chr_arith;
+	//char str_operand2[255];
+	char str_operand1[] = "II";
+	char chr_arith = '+';
+	char str_operand2[] = "II";
 	char *str_result;
 	char  c;
 	int   incr = 0;
@@ -25,110 +28,10 @@ void main() {
 	int   operand2;
 	int   result;
 	
-	printf("*****************Roman Calc*********************\n");
-	printf("Enter Arithmetic expression similar to the one shown below:\n");
-	printf("(Roman_Numeral_operand1 +/- Roman_Numeral_Operand2):\n");
-	//scanf("%s", str_calc);
-	
-	// Skip the initial white spaces or tabs
-	while((c = getchar()) == ' ' || c == '\t')
-	;
-	
-	if(c == '\n') {
-		printf("Only white space or tab enterd and no valid roman numerals entered\n");
-		return;
-	}else
-		str_operand1[incr++] = c; // Start of reading first operand
-	
-	while((c = getchar()) != ' ' && c != '\t' && c != '\n' && c != '+' && c != '-') {
-		if((c == 'M') || (c == 'D') || (c == 'C') || (c == 'L') || (c == 'X') || (c == 'V') || (c == 'I'))
-			str_operand1[incr++] = c;
-		else {
-			printf("Invalid roman numerals entered for operand 1\n");
-			return;
-		}
-	}
-	str_operand1[incr] = '\0';		// End of reading first operand
-	//printf("str-operand 1: %s\n", str_operand1);
-	
-	if(c == '\n') {
-		printf("Arithmetic Operators and Second Operand were not entered\n");
-		return;
-	}
-	
-	if(c == '+' || c == '-') {
-		chr_arith = c;
-	} else
-		while((c = getchar()) == ' ' || c == '\t')
-			;		// Skip white space or tab
-		
-	if(c == '+' || c == '-') {
-		chr_arith = c;
-	} else {
-		printf("Invalid arithmetic operator entered\n");
-		return;
-	}
-	//printf("Arithmetic operation: %c\n", chr_arith);
-	
-	incr = 0;  // Initialize the array count for second operand string
-	while((c = getchar()) == ' ' || c == '\t')
-	;		// Skip white space or tab
-	
-	if((c == '\n') || (c == '+') || (c ==  '-')) {
-		printf("No valid roman numeral operand 2 entered\n");
-		return;
-	}else
-		str_operand2[incr++] = c; // Start of reading second operand
-	
-	while((c = getchar()) != ' ' && c != '\t' && c != '\n') {
-		if((c == 'M') || (c == 'D') || (c == 'C') || (c == 'L') || (c == 'X') || (c == 'V') || (c == 'I'))
-			str_operand2[incr++] = c;
-		else {
-			printf("Invalid roman numerals entered for operand 2\n");
-			return;
-		}
-	}
-	str_operand2[incr] = '\0';		// End of reading second operand
-	//printf("str-operand2: %s\n",str_operand2);
-	
-	if(c != '\n') {
-		while((c = getchar()) == ' ' || c == '\t')
-		;		// Skip white space or tab
-	}
-	
-	if(c != '\n') {
-		printf("Invalid entries after entering two operands: %c\n", c);
-		return;
-	}
-	
-	// Convert roman numerals into decimal
-	operand1 = roman2dec(str_operand1);
-	operand2 = roman2dec(str_operand2);
-	
-	if(chr_arith == '+')
-		result = operand1 + operand2;   // Addition of two numbers
-	else {
-		if(operand1 > operand2)         // Difference between two numbers
-			result = operand1 - operand2;
-		else
-			result = operand2 - operand1;
-	}
-	
-	printf("Operand 1 & 2: %d & %d\n", operand1, operand2);
-	printf("Result: %d\n", result);
-	
-	if(result > 5000) {
-		printf("Result of the arithmetic operation is greater than 5000. ");
-		printf("Conversion of numbers greater than 5000 to Roman Numerals is not currently supported\n");
-	} else if(result < 1) {
-		printf("Result of the arithmetic operation is ZERO. ");
-		printf("Conversion of ZERO to Roman Numerals is not possible\n");
-	} else {
-		str_result = dec2roman(result);
-		
-		printf("Result in Roman Numerals: %s\n", str_result);
-		free(str_result);
-	}
+	//str_result = calc_parser();
+	str_result = roman_operation(str_operand1, str_operand2, chr_arith);
+	printf("%s\n", str_result);
+	free(str_result);
 }
 
 
